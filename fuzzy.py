cbae4343bae0afccc68f05e4e94940b99bd43336
Gabrielvd <gabrieldeoliveira4@gmail.com>
2024-07-04 23:14:13 -0300

    atualização das regras

--- fuzzy.py
+++ fuzzy.py
@@ -32,10 +32,25 @@ Duracao_Semaforo['media'] = fuzz.trimf(Duracao_Semaforo.universe, [0, 50, 100])
 Duracao_Semaforo['longa'] = fuzz.trimf(Duracao_Semaforo.universe, [50, 100, 100])
 
 # Definindo as regras
-regra1 = ctrl.Rule(Volume_Trafego['alto'] & Condicao_Estrada['ruim'] & Hora_Dia['manha'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
+regra1 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['manha'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
 regra2 = ctrl.Rule(Volume_Trafego['baixo'] & Condicao_Estrada['boa'] & Hora_Dia['madrugada'] & Dia_Semana['fim_semana'], Duracao_Semaforo['longa'])
 regra3 = ctrl.Rule(Volume_Trafego['medio'] & Condicao_Estrada['media'] & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['media'])
-regra4 = ctrl.Rule(Volume_Trafego['alto'] & Hora_Dia['noite'] & Dia_Semana['fim_semana'], Duracao_Semaforo['media'])
+regra4 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['noite'] & Dia_Semana['fim_semana'], Duracao_Semaforo['media'])
+regra5 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
+
+regras = []
+
+for vt in ['baixo', 'medio', 'alto']:
+    for ce in ['boa', 'media', 'ruim']:
+        for hd in ['madrugada', 'manha', 'tarde', 'noite']:
+            for ds in ['semana', 'fim_semana']:
+              if vt == 'alto' and ((hd == 'tarde' or hd == 'manha') and ds == 'semana' or (ds == 'fim_semana' and hd == 'noite')):
+                #--- duracao_semafaro = curta
+              elif vt == 'alto' and hd == 'madrugada' and ds == 'fim_semana':
+                #--duracao_semafaro = longa
+            
+              
+
 
 # Criando e simulando um controlador fuzzy
 sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4])
@@ -46,8 +61,6 @@ casos_teste = [
     {'Volume_Trafego': 8, 'Condicao_Estrada': 9, 'Hora_Dia': 8, 'Dia_Semana': 2},
     {'Volume_Trafego': 3, 'Condicao_Estrada': 1, 'Hora_Dia': 2, 'Dia_Semana': 5},
     {'Volume_Trafego': 5, 'Condicao_Estrada': 5, 'Hora_Dia': 15, 'Dia_Semana': 3},
-    {'Volume_Trafego': 10, 'Condicao_Estrada': 1, 'Hora_Dia': 15, 'Dia_Semana': 4},
-    {'Volume_Trafego': 1, 'Condicao_Estrada': 10, 'Hora_Dia': 5, 'Dia_Semana': 2},
 ]
 
 # Executando os casos de teste
