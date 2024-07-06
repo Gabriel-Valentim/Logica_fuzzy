515a214790bd3b770a5fabe085d8fa2c11785483
Gabrielvd <gabrieldeoliveira4@gmail.com>
2024-07-06 11:13:27 -0300

    atualização das regras

--- fuzzy.py
+++ fuzzy.py
@@ -15,9 +15,9 @@ Volume_Trafego['baixo'] = fuzz.trimf(Volume_Trafego.universe, [0, 0, 5])
 Volume_Trafego['medio'] = fuzz.trimf(Volume_Trafego.universe, [0, 5, 10])
 Volume_Trafego['alto'] = fuzz.trimf(Volume_Trafego.universe, [5, 10, 10])
 
-Condicao_Estrada['boa'] = fuzz.trimf(Condicao_Estrada.universe, [0, 0, 5])
+Condicao_Estrada['ruim'] = fuzz.trimf(Condicao_Estrada.universe, [0, 0, 5])
 Condicao_Estrada['media'] = fuzz.trimf(Condicao_Estrada.universe, [0, 5, 10])
-Condicao_Estrada['ruim'] = fuzz.trimf(Condicao_Estrada.universe, [5, 10, 10])
+Condicao_Estrada['boa'] = fuzz.trimf(Condicao_Estrada.universe, [5, 10, 10])
 
 Hora_Dia['madrugada'] = fuzz.trimf(Hora_Dia.universe, [0, 0, 6])
 Hora_Dia['manha'] = fuzz.trimf(Hora_Dia.universe, [6, 12, 12])
@@ -32,11 +32,11 @@ Duracao_Semaforo['media'] = fuzz.trimf(Duracao_Semaforo.universe, [0, 50, 100])
 Duracao_Semaforo['longa'] = fuzz.trimf(Duracao_Semaforo.universe, [50, 100, 100])
 
 # Definindo as regras
-regra1 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['manha'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
-regra2 = ctrl.Rule(Volume_Trafego['baixo'] & Condicao_Estrada['boa'] & Hora_Dia['madrugada'] & Dia_Semana['fim_semana'], Duracao_Semaforo['longa'])
-regra3 = ctrl.Rule(Volume_Trafego['medio'] & Condicao_Estrada['media'] & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['media'])
-regra4 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['noite'] & Dia_Semana['fim_semana'], Duracao_Semaforo['media'])
-regra5 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
+#regra1 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['manha'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
+#regra2 = ctrl.Rule(Volume_Trafego['baixo'] & Condicao_Estrada['boa'] & Hora_Dia['madrugada'] & Dia_Semana['fim_semana'], Duracao_Semaforo['longa'])
+#regra3 = ctrl.Rule(Volume_Trafego['medio'] & Condicao_Estrada['media'] & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['media'])
+#regra4 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['noite'] & Dia_Semana['fim_semana'], Duracao_Semaforo['media'])
+#regra5 = ctrl.Rule(Volume_Trafego['alto']  & Hora_Dia['tarde'] & Dia_Semana['semana'], Duracao_Semaforo['curta'])
 
 regras = []
 
@@ -44,22 +44,49 @@ for vt in ['baixo', 'medio', 'alto']:
     for ce in ['boa', 'media', 'ruim']:
         for hd in ['madrugada', 'manha', 'tarde', 'noite']:
             for ds in ['semana', 'fim_semana']:
-              if vt == 'alto' and ((hd == 'tarde' or hd == 'manha') and ds == 'semana' or (ds == 'fim_semana' and hd == 'noite')):
-                #--- duracao_semafaro = curta
-              elif vt == 'alto' and hd == 'madrugada' and ds == 'fim_semana':
-                #--duracao_semafaro = longa
-            
-              
+              if vt == 'alto' and (((hd == 'noite' or hd == 'manha') and ds == 'semana') or (ds == 'fim_semana' and hd == 'noite')):
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['curta']))
+
+              elif (vt == 'baixo' and hd == 'madrugada' and ds == 'fim_semana'):
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['longa']))
+
+              elif ce == 'ruim' and ((hd == 'noite' or hd == 'manha') and ds == 'semana') or (ds == 'fim_semana' and hd == 'noite'):
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['curta']))
+
+              elif vt != 'alto' and (ce == 'boa' or ce == 'media') and hd == 'madrugada':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['longa']))
+
+              elif vt == 'baixo' and ce == 'boa':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['longa']))
+
+              elif vt == 'medio' and ce == 'media':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['media']))
+
+              elif vt == 'alto' and ce == 'ruim':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['curta']))
+
+              elif vt == 'alto':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['curta']))
+
+              elif vt == 'medio':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['media']))
+
+              elif vt == 'baixo' or hd == 'tarde':
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['longa']))
+
+              else:
+                regras.append(ctrl.Rule(Volume_Trafego[vt] & Condicao_Estrada[ce] & Hora_Dia[hd] & Dia_Semana[ds], Duracao_Semaforo['media']))
+
 
 
 # Criando e simulando um controlador fuzzy
-sistema_controle = ctrl.ControlSystem([regra1, regra2, regra3, regra4])
+sistema_controle = ctrl.ControlSystem(regras)
 simulacao = ctrl.ControlSystemSimulation(sistema_controle)
 
 # Definindo um conjunto de casos de teste
 casos_teste = [
-    {'Volume_Trafego': 8, 'Condicao_Estrada': 9, 'Hora_Dia': 8, 'Dia_Semana': 2},
-    {'Volume_Trafego': 3, 'Condicao_Estrada': 1, 'Hora_Dia': 2, 'Dia_Semana': 5},
+    {'Volume_Trafego': 8, 'Condicao_Estrada': 8, 'Hora_Dia': 20, 'Dia_Semana': 2},
+    {'Volume_Trafego': 2, 'Condicao_Estrada': 1, 'Hora_Dia': 2, 'Dia_Semana': 5},
     {'Volume_Trafego': 5, 'Condicao_Estrada': 5, 'Hora_Dia': 15, 'Dia_Semana': 3},
 ]
 
